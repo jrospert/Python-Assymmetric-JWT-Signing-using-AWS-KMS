c71a8b2f1251668679ae920afeb5332e22484a05
Joel <joelrospert@gmail.com>
2023-02-06 15:36:51 -0800

    Updated sign and verify using AWS KMS sign() and verify() SDK

--- signJWT.py
+++ signJWT.py
@@ -1,49 +1,58 @@
 import base64
 import json
-import jwt
 import boto3
+from botocore.exceptions import ClientError
 
-def _jwt_kms_assemtric_encryption(jwt_head, jwt_payload, aws_key_arn):
-  
+# Encode the header and payload
+# return the string
+# return type: string, format: xxxxx.yyyyy
+def _create_jwt_header_payload(f_header, f_payload):
+
     # Dict containing "header" and "payload" - the first 2 of 3 parts of the JWT
-    # "header": takes the raw json, jwt_head, dumps into string, encodes it into UTF-8, 
-    #     decodes it back to its original contents in string form, strips any "=" chars,
-    #     and then base64encodes it all
-    # "payload": Does same operations as header
-    # json.dumps(): converts json to string
-    # encode(): encodes a json to UTF-8 format. E.g. (jwt_head).encode() = b'{"alg":"ES256"}
-    # decode(): decodes string into original contents. E.g. (jwt_head).encode().decode() = {"alg":"ES256"}
     token_components = {
-        "header": base64.urlsafe_b64encode(json.dumps(jwt_head).encode()).decode().rstrip("="),
-        "payload": base64.urlsafe_b64encode(json.dumps(jwt_payload).encode()).decode().rstrip("="),
+        "header": base64.urlsafe_b64encode(json.dumps(f_header).encode()).decode().rstrip("="),
+        "payload": base64.urlsafe_b64encode(json.dumps(f_payload).encode()).decode().rstrip("="),
     }
     message = f'{token_components.get("header")}.{token_components.get("payload")}'
+    return message
 
-    # Boto3 is the  Python SDK for AWS to interact with the AWS API
-    # Client class is boto3 client to represent AWS KMS
+# Sign the the header and payload
+# return the encoded digital signature
+# return type: string
+def _jwt_kms_asymmetric_sign(f_header_payload, aws_key_arn):      
+
+    kms_client = boto3.client('kms')  
+    try: 
+        sign = kms_client.sign(
+            KeyId = aws_key_arn,
+            Message = f_header_payload.encode(),
+            SigningAlgorithm="ECDSA_SHA_256"
+        )
+    except ClientError:
+        print("Could not create the digital signature")
+
+    else:
+        digital_signature = base64.urlsafe_b64encode(sign["Signature"]).decode()
+        return digital_signature
+
+# Verify the JWT signature
+# return if the signature is verified
+# return type: boolean
+def _jwt_kms_asymmetric_verify(message, signature, aws_key_arn):
+    message_encoded = bytes(message, 'utf-8')    
+    jwt_signature = bytes(base64.urlsafe_b64decode(signature))   
     kms_client = boto3.client('kms')
-    
-    # Client.sign() expects 'KeyId'='string', Message=b'bytes', MessageType='RAW'|'DIGEST',
-    #     SigningAlgorithm='RSASSA_PSS_SHA_256'|'ECDSA_SHA_256' and more.
-    # NOTE: Case matters
-    # Documentation here: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kms.html#KMS.Client.sign
-    response = kms_client.sign(
+    isVerified = kms_client.verify(
         KeyId = aws_key_arn,
-        Message = message.encode(),
-        MessageType = "RAW",
+        Message = message_encoded,
+        Signature = jwt_signature,
         SigningAlgorithm="ECDSA_SHA_256"
     )
-
-    # Client response returns is in binary
-    # Encode the binary to Base64URL encoding
-    token_components["signature"] = base64.urlsafe_b64encode(response["Signature"]).decode()
-
-    # Return the JWT
-    return f'{token_components.get("header")}.{token_components.get("payload")}.{token_components["signature"]}'
+   
+    return(isVerified["SignatureValid"])
 
 if __name__ == "__main__":
-    aws_kms_key_arn = "KMS KEY ARN address"
-    public_key_file_path = "Local location of public key"
+    aws_kms_key_arn = "arn:aws:kms:us-east-1:394485393101:key/caf01cef-0498-4b85-9268-fbb6f978d097"
     
     # Create the JWT Header
     header = {
@@ -53,17 +62,19 @@ if __name__ == "__main__":
 
     # Create the JWT Payload
     payload = {
-        "user_id": "Joel"
+        "user_id": "Bob"
     }
 
-    # Sign the JWT with the private key
-    jwt_encoded = _jwt_kms_assemtric_encryption(header, payload, aws_kms_key_arn)
-    
-    # Open the public key for JWT verification
-    key = ""
-    with open(public_key_file_path, "r") as f:
-       key = f.read()
+    # Create the message (header + payload) to be signed
+    jwt_header_payload = _create_jwt_header_payload(header, payload)
 
-    # Verify the JWT signature
-    decoded_data = jwt.decode(jwt_encoded, key, algorithms=["ES256"])
-    print(decoded_data) 
+    # Sign the JWT with the private key
+    jwt_signature = _jwt_kms_asymmetric_sign(jwt_header_payload, aws_kms_key_arn)    
+  
+    # Fully signed JWT
+    jwt_full = (jwt_header_payload + "." + jwt_signature).rstrip("=")
+    print("The signed JWT is: " + jwt_full)
+    
+    # Verify the JWT
+    isVerified = _jwt_kms_asymmetric_verify(jwt_header_payload, jwt_signature, aws_kms_key_arn)
+    print("Is the signature valid? " + str(isVerified))
